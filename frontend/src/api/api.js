944f906b8fcfbfe509e35250440e173b5f2e6e11
Arshlaan haque <arshlaanhaq005@gmail.com>
2025-02-15 17:20:43 +0530

    added backend url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios  from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://student-interaction-k8xk.onrender.com/api',
 })
 API.interceptors.request.use((req) => {
         const token = localStorage.getItem('token')
